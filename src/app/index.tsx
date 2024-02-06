ba7be68a9f20b3a6c54817c6fba512eb420fd6b4
leonardo <leo-feitosa@live.com>
2024-02-06 17:30:19 -0300

    feat: add header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
+import { Header } from "../components/header";
 
 export default function Home() {
   return (
     <View className="bg-slate-900 flex-1 pt-8">
-      <Text className="text-white text-2xl">Hello react native</Text>
+      <Header title="Faça o seu pedido" cardQuantityItems={1}/>
     </View>
   );
 }

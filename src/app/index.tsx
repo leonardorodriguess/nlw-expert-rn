bd23073f814aedddb9bae9846242b9c3d10375ea
leonardo <leo-feitosa@live.com>
2024-02-06 18:41:42 -0300

    feat: add list  button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,16 +3,29 @@ import { Header } from "@/components/header";
 import { FlatList, View } from "react-native";
 
 import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="bg-slate-900 flex-1 pt-8">
       <Header title="Faça o seu pedido" cardQuantityItems={1} />
 
-      <FlatList 
+      <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({item }) => <CategoryButton title={item} />}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
         horizontal
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}

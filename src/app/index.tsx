0852aec0959ed34d3cbd0838d967187a8842bb29
leonardo <leo-feitosa@live.com>
2024-02-06 17:34:45 -0300

    config: alter paths defalt

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from "react-native";
-import { Header } from "../components/header";
+import { Header } from "@/components/header";
+import { View } from "react-native";
 
 export default function Home() {
   return (
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export function Header({ title, cardQuantityItems= 0 }: HeaderProps) {
   return (
     <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
       <View className="flex-1">
-        <Image source={require("@/src/assets/logo.png")} className="h-6 w-32" />
+        <Image source={require("@/assets/logo.png")} className="h-6 w-32" />
 
         <Text className="text-white  text-xl font-heading mt-2">{title}</Text>
       </View>
